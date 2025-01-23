9250549018ed8c953210f354e439f7b170732efa
BaronKoy <bkero1991@gmail.com>
2025-01-23 12:38:18 +0000

    directory change

--- Code/r/focal.r
+++ Code/r/focal.r
@@ -4,7 +4,7 @@ library(ggplot2)
 library(reshape2)
 options(scipen = 999) # prevent scientific notation for genomic position on x axis
 
-population <- read_csv('/home/baron/Documents/PhD/ML_balancingselection_data/reuter_data/abi_pileups/focal_outputs/columns/cage_1.csv',
+population <- read_csv('/home/baron/Documents/PhD/ML_balancingselection_data/reuter_data/abi_pileups/focal_outputs/columns/matches/cage_1.csv',
                    col_types = cols(Position = col_number(), Allele_frequency = col_number(),
                                     Generation = col_character()))
 #View file if required
