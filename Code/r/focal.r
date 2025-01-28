cc77c24daf7538a0844c2d1736ee9cff36aa563a
BaronKoy <bkero1991@gmail.com>
2025-01-28 11:55:09 +0000

    line of best fit added to plots

--- Code/r/focal.r
+++ Code/r/focal.r
@@ -11,7 +11,10 @@ population <- read_csv('/home/baron/Documents/PhD/ML_balancingselection_data/reu
 #View(allele)
 
 cage_plot <- ggplot(population, aes(x = Position, y = Allele_frequency, color = Generation)) +
-  geom_count(size = 2) + scale_color_discrete(breaks = c(2,4,8,12,20,28,36,44,56)) +
+  geom_count(size = 3) +
+  geom_line(size = 1) +
+  #geom_smooth(method = 'lm', se = FALSE) +
+  scale_color_discrete(breaks = c(2,4,8,12,20,28,36,44,56)) +
   theme_grey() 
   labs(title = 'Alternate allele frequency within focal region (18520501-18521500) - Cage 1', y = 'Alternate allele frequency', x = 'Genomic position (Chromosome 3R)')
   theme(axis.text = element_text(size = 15), axis.title = element_text(size = 15))
