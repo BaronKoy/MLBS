d8ff597b542774c58c971078cde12338bb91b809
BaronKoy <bkero1991@gmail.com>
2025-02-05 20:03:19 +0000

    frequency script updates

--- Code/bash/Cage_frequencies.sh
+++ Code/bash/Cage_frequencies.sh
@@ -1,17 +1,20 @@
-# Cut original pileup files to only include focal region 3R:18520501 - 18521500
-# TODO...currently the script needs to be run from the same working directory the data files reside in
-# For the love of god fix the newline shell syntax in this
+# Code to cut original pileup files to only include focal region 3R:18520501 - 18521500
+# Retrives position, minor allele frequency, & adds generation to each file
 
+# TODO...finish final step that concatenates final output files together and adds column headers
+# TODO...currently the script needs to be run from the same working directory the pileup files reside in
+
+# Extract lines for focal region and output to new file
 echo 'Cutting mpileup files for focal region 3R:28520501-18521500......'
 for x in *.gz ; do zcat "$x" | sed -rn '/^3R/p' | awk '/18520501/,/18521500/' > "$x".pileup ; done
 echo Region cut complete!
 
-# Run ngsPool for allele frequencies
+# Run ngsPool to calculate minor allele frequencies
 echo 'Running ngsPool......'
 for x in *.pileup ; do julia ~/software/ngsJulia/ngsPool/ngsPool.jl --fin "$x" --fout ngs_output/"$x".out.gz --nChroms 96 ; done
-echo ngsPool process complete!
+echo Allele frequencies calculated!
 
-# Extract colum 2 (Position) & column 11 (minor allele frequency)
+# Extract colum 2 (Position) & column 11 (minor allele frequency) and output to new file
 echo 'Extracting columns 2 & 11 for position and minor allele frequency......'
 cd ngs_output/
 for x in *.out.gz ; do zcat "$x" | awk '{print$2,$11}' > plot_data/"$x" ; done
@@ -34,6 +37,7 @@ for x in S??0918_* ; do sed -i 's/$/,28/g' "$x" ; done
 for x in S??0119_* ; do sed -i 's/$/,36/g' "$x" ; done
 for x in S??0519_* ; do sed -i 's/$/,44/g' "$x" ; done
 for x in S??1119_* ; do sed -i 's/$/,56/g' "$x" ; done
+echo 'Generation numbers added to output files!'
 
 # Removing non-variant lines
 echo 'Removing non-variant lines......'
@@ -42,4 +46,4 @@ for x in *.out.gz ; do sed -i '/position,saf_MLE/d' "$x" ; done
 echo 'Non-variant lines removed!'
 
 # Combine into a single plot file
-#TODO...
+#TODO...concatenates final files into plot data file and add column headers for plot script...
