b6a7bbae94f845a8a88b75baaeef62565a8d837b
BaronKoy <bkero1991@gmail.com>
2025-02-09 23:59:44 +0000

    stat script update

--- Code/r/focal.r
+++ Code/r/focal.r
@@ -2,23 +2,25 @@
 library(readr)
 library(ggplot2)
 library(reshape2)
+library(gghighlight)
 options(scipen = 999) # prevent scientific notation for genomic position on x axis
 
-population <- read_csv('/home/baron/Documents/PhD/ML_balancingselection_data/reuter_data/abi_pileups/focal_outputs/plot_data/region_data_1.csv',
+population <- read_csv('/home/baron/Documents/PhD/ML_balancingselection_data/abi_raw_data/ngs_output/plot_data/final/region_data_2.csv',
                    col_types = cols(Position = col_character(), Allele_frequency = col_number(),
                                     Generation = col_number()))
+focal <- population$Position['18521001']
 #View file if required
 #View(allele)
 
-cage_plot <- ggplot(population, aes(x = Generation, y = Allele_frequency, color = Position)) +
+# General plot for all variants within focal region
+ggplot(population, aes(x = Generation, y = Allele_frequency, color = Position)) +
   #geom_count(size = 3) +
-  geom_line(size = 0.5) +
+  geom_line(linewidth = 0.5) +
   #geom_line(data = population['Position'[18521001]]) +
   #geom_smooth(method = 'lm', se = FALSE) +
   #scale_color_discrete(breaks = c(2,4,8,12,20,28,36,44,56)) +
   theme_grey() +
+  #scale_color_gradient2(low = 'black', mid = 'yellow', high = 'blue')
   labs(title = 'Alternate allele frequency within focal region (18520501-18521500) - Cage 1', y = 'Alternate allele frequency', x = 'Generation') +
   scale_x_continuous(breaks = c(2,4,8,12,20,28,36,44,56)) +
   theme(axis.text = element_text(size = 15), axis.title = element_text(size = 15))
-  
-cage_plot
